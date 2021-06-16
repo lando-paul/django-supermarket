0395d62952f7f8a846ea9c41f3f8cf81defe697c
Ida Delphine <idadelm@gmail.com>
2021-06-16 21:04:57 +0100

    Added product and checkout views

--- catalog/views.py
+++ catalog/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def product(request):
+    return render(request, 'product.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
